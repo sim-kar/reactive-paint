82376d753305aa180d201636f74389b95412815a
Simon Karlsson <sika2001@student.miun.se>
2022-09-23 15:48:51 +0200

    Implemented Rectangle

--- src/se/miun/dt176g/sika2001/reactive/Rectangle.java
+++ src/se/miun/dt176g/sika2001/reactive/Rectangle.java
@@ -4,24 +4,25 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 /**
- * <h1>ConcreteShape</h1> Creates a Circle-object.
- * Concrete class which extends Shape.
- * In other words, this class represents ONE type of shape
- * i.e. a circle, rectangle, n-sided regular polygon (if that's your thing)
- *
- * @author 	--YOUR NAME HERE--
- * @version 1.0
- * @since 	2022-09-08
+ * <h1>Rectangle</h1>
+ * Creates a Rectangle with one corner at the given start {@link Point} and the opposite corner at
+ * the end {@link Point}.
  */
-
 public class Rectangle extends Shape {
+
+	/**
+	 * Create a new rectangle with opposing corners at the given {@link Point}s.
+	 *
+	 * @param start position of the first corner.
+	 * @param end position of the second corner (opposite the first).
+	 */
+	public Rectangle(Point start, Point end) {
+		super(start, end);
+	}
 
 	@Override
 	public void draw(Graphics g) {
-		Graphics2D g2 = (Graphics2D) g; // Type-cast the parameter to Graphics2D.
-		   
-		// Draw using g2.
-		// eg g2.fillOval(int x, int y, int width, int height)
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawRect(this.topLeft().x(), this.bottomRight().y(), this.width(), this.height());
 	}
-
 }
