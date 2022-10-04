423e8072ad25c3b950f61f45ed2e5b53ff7c6d9d
Simon Karlsson <sika2001@student.miun.se>
2022-10-04 15:09:36 +0200

    Added javadoc to Point

--- src/se/miun/dt176g/sika2001/reactive/Point.java
+++ src/se/miun/dt176g/sika2001/reactive/Point.java
@@ -1,36 +1,56 @@
 package se.miun.dt176g.sika2001.reactive;
 
-
-
 /**
  * <h1>Point</h1> 
- *
- * @author 	--YOUR NAME HERE--
- * @version 1.0
- * @since 	2022-09-08
+ * A point on a 2D plane with an x- and a y-coordinate.
  */
-
 public class Point {
 
 	private int x, y;
 
+	/**
+	 * Contracts a new Point with the given coordinates.
+	 *
+	 * @param x the x-coordinate
+	 * @param y the y-coordinate
+	 */
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
+	/**
+	 * Get the x-coordinate of this Point.
+	 *
+	 * @return the x-coordinate
+	 */
 	public int x() {
 		return x;
 	}
 
+	/**
+	 * Get the y-coordinate of this Point.
+	 *
+	 * @return the y-coordinate.
+	 */
 	public int y() {
 		return y;
 	}
 
+	/**
+	 * Set a new x-coordinate for this Point.
+	 *
+	 * @param x the new x-coordinate.
+	 */
 	public void x(int x) {
 		this.x = x;
 	}
 
+	/**
+	 * Set a new y-coordinate for this Point.
+	 *
+	 * @param y the new y-coordinate
+	 */
 	public void y(int y) {
 		this.y = y;
 	}
