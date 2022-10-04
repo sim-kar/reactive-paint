4b474e07a09031e140ef3608bbc40673a223b77c
Simon Karlsson <sika2001@student.miun.se>
2022-10-04 15:00:40 +0200

    Added javadoc to DrawingPanel

--- src/se/miun/dt176g/sika2001/reactive/DrawingPanel.java
+++ src/se/miun/dt176g/sika2001/reactive/DrawingPanel.java
@@ -4,33 +4,44 @@ import java.awt.*;
 import javax.swing.*;
 
 /**
- * <h1>DrawingPanel</h1> Creates a Canvas-object for displaying all graphics
- * already drawn.
- *
- * @author 	--YOUR NAME HERE--
- * @version 1.0
- * @since 	2022-09-08
+ * <h1>DrawingPanel</h1>
+ * A Canvas-object for displaying all graphics already drawn.
  */
 
 @SuppressWarnings("serial")
 public class DrawingPanel extends JPanel {
 
 	private Drawing drawing;
-	
 
+	/**
+	 * Constructs a new DrawingPanel.
+	 */
 	public DrawingPanel() {
 		drawing = new Drawing();
 	}
-	
+
+	/**
+	 * Redraw this DrawingPanel. Will redraw the set {@link Drawing}.
+	 */
 	public void redraw() {
 		repaint();
 	}
 
+	/**
+	 * Set a {@link Drawing} for this panel to display.
+	 *
+	 * @param d the Drawing to display
+	 */
 	public void setDrawing(Drawing d) {
 		drawing = d;
 		repaint();
 	}
 
+	/**
+	 * Get the {@link Drawing} that this panel is displaying.
+	 *
+	 * @return the currently displayed drawing
+	 */
 	public Drawing getDrawing() {
 		return drawing;
 	}
