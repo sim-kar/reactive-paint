1cee4ce61d43c8a4a786f0b9183de573a6932fe0
Simon Karlsson <sika2001@student.miun.se>
2022-10-25 14:56:39 +0200

    Added documentation to Client

--- src/se/miun/dt176g/sika2001/reactive/Client.java
+++ src/se/miun/dt176g/sika2001/reactive/Client.java
@@ -5,12 +5,22 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+/**
+ * A client for writing and reading objects to and from a server. Acts as a wrapper for the given
+ * socket.
+ */
 public class Client {
 
     private final Socket clientSocket;
     private final ObjectInputStream input;
     private final ObjectOutputStream output;
 
+    /**
+     * Create a new client with the given socket.
+     *
+     * @param clientSocket the socket to use
+     * @throws IOException if an I/O error occurs when getting the socket's input and output streams
+     */
     public Client(Socket clientSocket) throws IOException {
         this.clientSocket = clientSocket;
 
@@ -19,10 +29,23 @@ public class Client {
         this.input = new ObjectInputStream(clientSocket.getInputStream());
     }
 
+    /**
+     * Read an object from the connected server.
+     *
+     * @return the object
+     * @throws IOException if an I/O error occurs when reading the object
+     * @throws ClassNotFoundException if the class of the object being read can't be found
+     */
     public Object read() throws IOException, ClassNotFoundException {
         return input.readObject();
     }
 
+    /**
+     * Write an object to the connected server.
+     *
+     * @param object the object to write
+     * @throws IOException if an I/O error occurs when writing the object
+     */
     public void write(Object object) throws IOException {
         output.writeObject(object);
     }
@@ -35,10 +58,20 @@ public class Client {
         return this.output;
     }
 
+    /**
+     * Close the socket of this client.
+     *
+     * @throws IOException if an I/O error occurs when closing the socket
+     */
     public void shutdown() throws IOException {
         clientSocket.close();
     }
 
+    /**
+     * Check whether this client has been shutdown, i.e. its socket has been closed.
+     *
+     * @return whether the client is shutdown
+     */
     public boolean isShutdown() {
         return clientSocket.isClosed();
     }
